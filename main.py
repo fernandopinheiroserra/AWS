c517cb0c678d3f1ddf7071b2681d7a6dbff56447
Fernando Pinheiro Serra <fernandogitserra@gmail.com>
2023-07-06 14:22:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,18 @@
 
 def main():
     pass
+
+
+def PracticionerExame():
+    reason1 = "Validates overall understanding of the AWS Cloud"
+    reason2 = "It´s a Industry-recognized credencial validating knowledge"
+
+    prereq1 = "Anyone who works with the AWS Cloud"
+    prereq2 = "At least six months of experience with the AWS Cloud and basic understanding of IT services recommended" 
+
+    cost = "$100 per exame"
+    time = "90 minutes of multiple-choice questions on a test center or remotely at home"
+
+    domains = ["Cloud Concepts", "Security and Compliance", "Technology", "Billing and Pricing"]
+
+    
