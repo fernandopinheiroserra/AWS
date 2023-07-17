b0acd565e86e2342444b2bc121c646179e2d7aff
Fernando Pinheiro Serra <fernandogitserra@gmail.com>
2023-07-17 19:20:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,4 +30,24 @@ def cloudComputing():
     exampleoldstructure = ['seach for server', 'find server', 'Ger Quote', 'Send Quote to Finance', 'Get Approved', 'Finance Pays', 'Order Server (pay now)', 'validation: did it fit the needs?']
     collaborationfeatures = ['Hold virtual meetings', 'Edit documents together', 'Communicate with messaging services', 'Send emails']
     techfeatures = ['Instantaneous access', 'Access resources where and when you want', 'More flexible and affordable than legacy IT infraestreucture', 'Affordable for organizations and individuals with smaller budgets']
-    history = ['1950-Mainframe', '1970-Virtual machines', ]
+    history = ['1950-Mainframe', '1970-Virtual machines']
+    shiftToCloudConcept = 'What if we could use lot´s of mainframe computers resources as if it´s just one computer?'
+    hypervisor = 'Software that could be installed onto multiple pieces of hardware, such as a server, linking all the hardwares and use their computational storage powers as one giant resource'
+
+
+def anotherDayAnotherCloud():
+    usingthecloud = ['e-mail', 'streaming services', 'office365','google cloud']
+
+
+def cloudComputingModels():
+    saas = ['Software as a Service', 'Completed products managed by the service provider. we get the hole package of the service complete with user interfaces. Ready for use, regardless of their technical backgrounds', 'Outlook, Gmail', 'on note, positive: don´t need to worry about the infrastructure is maintained or managed' ]
+    paas = ['Platform as a Service', 'Allows to deploy and manage applications without worrying about the underlying hardware infrastructure', 'services offered: webservers, databases, operating systems or environments where we can execute specific programming languages to host applications', 'examples: Microsoft Azure web hosting, Google app engine and Heroku', 'on note, negative: its diferent from Iaas, because packages ar pre-constructed, positive: less time spending in maintenance and configuration of the infrastructure']
+    iaas = ['Infrastructure as a Service', 'Refers to the Basic building blocks of cloud it Infrastructure', 'control over networking, security, computer and servers', 'provides the most flexibility and management control of all the types of cloud computing models', 'closest to on-premise datacenter in features', 'examples: Amazon webservices, microsoft azure, google cloud']
+    complexitylevel = ['iaas', 'paas', 'saas']
+
+def cloudDeployments():
+    concept = 'when a organization utilizes cloud deployment, it means that all parts of its IT infrastructure reside and run on the cloud'
+    cloudDeployments = []
+    onPremisesdeployments = []
+    hybridDeployments = []
+
