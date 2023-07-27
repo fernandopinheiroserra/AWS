8c842bb1dc0d1c97e5800ca96a67ae8c882ed91a
Fernando Pinheiro Serra <fernandogitserra@gmail.com>
2023-07-27 08:27:49 -0300

    security

---
+++ securityAndCompliance.py
@@ -0,0 +1,24 @@
+
+
+def goals():
+    ableTodescribe = ['key services on the AWS platform and their common use cases', 'Basic security and compliance aspects of the AWS platform and the shared security model', 'Basic/core characteristics of deploying and operating in the AWS Cloud']
+    ableToIdentify = 'Sources of documentation or technical assistance'
+
+
+def secondDomain():
+    examGoal = ['Makes up 25% of the exam', 
+                'Define the AWS shared responsibility model',
+                'Define the AWS Cloud security and compliance concepts'
+                'Identify AWS access management capabilities'
+                'Identify resources for receiving security related support']
+    
+def infraestructurelooklike():
+    nowAdays = ['Global network of data centers built with security in mind'
+                'Safeguards to protect customer privacy'
+                'Dozens of compliance programs to help meet industry compliance requirements for data security']
+     
+    awsAlowsyouTo = ['High-security standards without need for your own data centers', 'Scale your business quickly']
+
+
+def securityResponsabilities():
+    sharedResponsabilityModel = []
